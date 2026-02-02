5e20f6812d5c760d32ac336a2dd0145c96d714f5
Kurt King <kurtaking@gmail.com>
2026-02-02 13:32:39 -0700

    lint on commit
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,13 +1,13 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('server')
-    .setDescription('Provides information about the server.'),
-  async execute(interaction) {
-    // interaction.guild is the object representing the Guild in which the command was run
-    await interaction.reply(
-      `This server's name is ${interaction.guild.name} and has a total of ${interaction.guild.memberCount} members.`,
-    );
-  },
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides information about the server.'),
+	async execute(interaction) {
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply(
+			`This server's name is ${interaction.guild.name} and has a total of ${interaction.guild.memberCount} members.`,
+		);
+	},
 };
