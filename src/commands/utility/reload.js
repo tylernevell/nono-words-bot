0973db7cde23c78e7fb5ae7a56ddbe81524558e8
Tyler Nevell <tylernevell@gmail.com>
2026-02-03 16:33:39 -0500

    reset reload

--- src/commands/utility/reload.js
+++ src/commands/utility/reload.js
@@ -2,39 +2,39 @@ import { SlashCommandBuilder } from 'discord.js';
 
 // The reload command ideally should not be used by every user. You should deploy it as a guild command in a private guild.
 export default {
-  data: new SlashCommandBuilder()
-    .setName('reload')
-    .setDescription('Reloads a command.')
-    .addStringOption((option) =>
-      option
-        .setName('command')
-        .setDescription('The command to reload.')
-        .setRequired(true),
-    ),
-  async execute(interaction) {
-    const commandName = interaction.options
-      .getString('commnad', true)
-      .toLowerCase();
-    const command = interaction.client.commands.get(commandName);
+	data: new SlashCommandBuilder()
+		.setName('reload')
+		.setDescription('Reloads a command.')
+		.addStringOption((option) =>
+			option
+				.setName('commnad')
+				.setDescription('The command to reload.')
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const commandName = interaction.options
+			.getString('commnad', true)
+			.toLowerCase();
+		const command = interaction.client.commands.get(commandName);
 
-    if (!command) {
-      return interaction.reply(
-        `There is no command with name \`${commandName}\`!`,
-      );
-    }
+		if (!command) {
+			return interaction.reply(
+				`There is no command with name \`${commandName}\`!`,
+			);
+		}
 
-    try {
-      const newCommand = (await import(`./${command.data.name}.js`)).default;
-      interaction.client.commands.set(newCommand.data.name, newCommand);
-      await interaction.reply(
-        `Command \`${newCommand.data.name}\` was reloaded!`,
-      );
-    }
- catch (error) {
-      console.error(error);
-      await interaction.reply(
-        `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``,
-      );
-    }
-  },
+		try {
+			const newCommand = (await import(`./${command.data.name}.js`)).default;
+			interaction.client.commands.set(newCommand.data.name, newCommand);
+			await interaction.reply(
+				`Command \`${newCommand.data.name}\` was reloaded!`,
+			);
+		}
+		catch (error) {
+			console.error(error);
+			await interaction.reply(
+				`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``,
+			);
+		}
+	},
 };
